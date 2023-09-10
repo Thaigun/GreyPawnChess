af8125a3159dd82b901ddcc1a4f941836934e19a
Antti <makipaa.antti@gmail.com>
2023-09-10 22:28:38 +0200

    One more test to try simple forced mate.

--- addon/engine/test/MonteCarloNodeTest.cpp
+++ addon/engine/test/MonteCarloNodeTest.cpp
@@ -19,7 +19,7 @@ TEST(MonteCarloNodeTest, FirstIteration)
     MonteCarloNode root;
     Board freshBoard;
 
-    root.runIteration(freshBoard, 0);
+    root.runIteration(freshBoard, 0u);
     Move bestMove = root.highestWinrateMove();
     EXPECT_EQ(root.nodeVisits(), 1u);
     MonteCarloNode nextNode = root.getNodeForMove(bestMove);
@@ -30,10 +30,23 @@ TEST(MonteCarloNodeTest, ManyIterations)
 {
     MonteCarloNode root;
     Board board;
-    unsigned int iterations = 1000;
+    unsigned int iterations = 1000u;
     for (unsigned int i = 0u; i < iterations; i++)
     {
-        root.runIteration(board, root.nodeVisits());
+        root.runIteration(board, 50u);
     }
     EXPECT_EQ(root.nodeVisits(), iterations);
+}
+
+TEST(MonteCarloNodeTest, ForcedMate)
+{
+    MonteCarloNode root;
+    Board forcedMateInTwo = Board::buildFromFEN("2r4k/6pp/5p2/7K/2R1r3/q4n2/2R5/8 w - - 0 1");
+    unsigned int iterations = 1000u;
+    for (unsigned int i = 0u; i < iterations; i++)
+    {
+        root.runIteration(forcedMateInTwo, 50u);
+    }
+    const Move bestMove = root.highestWinrateMove();
+    EXPECT_EQ(bestMove.asUCIstr(),"c4c8");
 }
